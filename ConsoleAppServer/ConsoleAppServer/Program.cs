0d43e8da131dda3caf03008db143b8dff47f5255
Jamshid Eftekhari <jaef@easj.dk>
2019-09-02 15:35:18 +0200

    simple server and client

--- ConsoleAppServer/ConsoleAppServer/Program.cs
+++ ConsoleAppServer/ConsoleAppServer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
 
 namespace ConsoleAppServer
 {
@@ -6,7 +8,36 @@ namespace ConsoleAppServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+            Console.WriteLine("server started witing for connection!");
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            //Socket connectionSocket = serverSocket.AcceptSocket();
+            Console.WriteLine("Server activated");
+
+            Stream ns = connectionSocket.GetStream();
+            // Stream ns = new NetworkStream(connectionSocket);
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            connectionSocket.Close();
+            serverSocket.Stop();
         }
     }
 }
---
+++ ConsoleAppServer/ConsoleAppTcpClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ConsoleAppTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World it is client!");
+            Console.ReadLine();
+            TcpClient clientSocket = new TcpClient("localhost", 6789);
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string serverAnswer = sr.ReadLine();
+            Console.WriteLine("Server: " + serverAnswer);
+
+            ns.Close();
+
+            clientSocket.Close();
+        }
+    }
+}
