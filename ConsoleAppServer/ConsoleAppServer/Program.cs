91adabe2ab03de4c0291f486293dd30800a72b87
Jamshid Eftekhari <jaef@easj.dk>
2019-09-02 13:58:24 +0200

    first version

---
+++ ConsoleAppServer/ConsoleAppServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
